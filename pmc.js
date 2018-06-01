080143092c2d38bdddc3fa50e0b68deab4dacfc9
Fred Lintz <fred.lintz5@gmail.com>
2018-06-01 00:19:00 -0600

    rmeoved file

--- pmc.js
+++ pmc.js
@@ -178,7 +178,7 @@ $('#submitProjectedTSS').on('click', function() {
 	let inputArray = [];
 	let inputs = $('#nav-projected :input').not(':input[type=button]');
 
-	inputs.each(() => inputArray.push({date: $(this).attr('id'), tss: $(this).val()}));
+	inputs.each(() => inputArray.push({date: $(value).attr('id'), tss: $(value).val()}));
 	
 	createProjectedChart(inputArray);
 

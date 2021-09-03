226249711d10fb5ff991b57cf6fea1558b2fdcbc
Fred Lintz <fred.lintz5@gmail.com>
2021-09-02 21:37:48 -0600

    no moar fit to jar to csv to json conversions

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-const csvFilePath = 'fredTest2.csv'
-const csv = require('csvtojson')
+// const csvFilePath = 'fredLapTest.csv'
+// const csvFilePath = 'fredTest2.csv'
+// const csv = require('csvtojson')
 const _get = require('lodash.get')
+const fitDecoder = require('fit-decoder');
+const fs = require('fs');
+const { parseRecords } = require('./fitDecoder/semantics');
 
 const functional_threshold_power = 240
 const maxInteger = Number.MAX_SAFE_INTEGER
@@ -16,13 +20,66 @@ const maxInteger = Number.MAX_SAFE_INTEGER
  * TSS = (durationOfWorkoutInSeconds * NP * IF) / (FTP * 36)
 */
 
-function getCorrectFuckingValue(map, keys, label) {
-  try {
-    return map[`Value ${keys.find(k => map[k] === label).split(' ')[1]}`]
-  } catch(e) {
-    return ''
-  }
-}
+fs.readFile('./fredLapTest.fit', (err, data) => {
+  const startTime = Date.now()
+  
+  const jsonRaw = fitDecoder.fit2json(data.buffer);
+
+  /*
+    data: {
+      ascent: 18,
+      grade: -391,
+      gps_accuracy: 2,
+      latitude: 39.73923,
+      longitude: -105.204762,
+      timestamp: 998955723000, (ms)
+      cadence: 84,             (rpm)
+      power: 154,              (watts)
+      altitude: 11399,         (meters)
+      temperature: 25,         (celsius)
+      speed: 6.11,             (meters/s)
+      distance: 107233,        (meters/s)
+      left_right_balance: 46,  (percentage)
+      calories: 36,            (kilocalories)
+      heart_rate: 136,         (beats per minute)
+    },
+    type: 'record',
+  */
+  const jsonParsed = parseRecords(jsonRaw);
+  
+  let nestedArray = []
+
+  /*
+   * chunked records shape: array of laps. each lap is an array of records
+   * [
+   *   [{}, {}, {}, {}, {}]
+   *   [{}, {}, {}, {}, {}]
+   * ]
+  */
+  const chunkedRecords = jsonParsed.records
+    .filter(row => ['record', 'lap'].includes(row.type))
+    .reduce((acc, { type, data }) => {
+      if (type === 'lap') {
+        const accumulatorPlusNestedArray = acc.length ? [...acc, nestedArray] : [nestedArray]
+        nestedArray = []
+        return accumulatorPlusNestedArray
+      }
+      
+      nestedArray.push(data)
+      return acc
+    }, []);
+
+  const per_lap_workout_data = chunkedRecords.reduce((acc, lap, index) => ({ ...acc, [`Lap ${index + 1}`]: getWorkoutData(lap) }), {})
+
+  const total_workout_data = getWorkoutData(chunkedRecords.reduce((acc, lap, index) => index === 0 ? [...acc, ...lap] : [...lap], []))
+
+  const endTime = Date.now()
+
+  console.log('per_lap_workout_data', JSON.stringify(per_lap_workout_data, null, 2))
+  console.log('total_workout_data', JSON.stringify(total_workout_data, null, 2))
+  console.log(`${total_workout_data.length} records in ${endTime - startTime} milliseconds`);
+})
+
 
 function minAvgMaxCalculations(accumulator, fields, currentIndex, finalIndex, recordsLength) {
   return Object.keys(fields).reduce((acc, field) => {
@@ -37,49 +94,14 @@ function minAvgMaxCalculations(accumulator, fields, currentIndex, finalIndex, re
   }, {})
 }
 
-csv()
-.fromFile(csvFilePath)
-.then((rows) => {
-  const semiCircleConversion = 180 / 2 ** 31
-  
-  const startTime = Date.now()
-  
-  const records = rows
-    .filter(row => row.Type === 'Data' && row.Message === 'record')
-    .map(r => {
-      const keys = Object.keys(r)
-
-      return {
-        // timestamp in milliseconds
-        timestamp: getCorrectFuckingValue(r, keys, 'timestamp') * 1000,
-        latitude: getCorrectFuckingValue(r, keys, 'position_lat') * semiCircleConversion,
-        longitude: getCorrectFuckingValue(r, keys, 'position_long') * semiCircleConversion,
-        // distance in meters/s
-        distance: Number(getCorrectFuckingValue(r, keys, 'distance')),
-        // altitude in meters
-        altitude: Number(Number(getCorrectFuckingValue(r, keys, 'altitude')).toFixed()),
-        // heart_rate in beats per minute
-        heart_rate: Number(getCorrectFuckingValue(r, keys, 'heart_rate')),
-        // calories in kilocalories
-        calories: Number(getCorrectFuckingValue(r, keys, 'calories')),
-        // cadence in rpm
-        cadence: Number(getCorrectFuckingValue(r, keys, 'cadence')),
-        // speed in meters/s
-        speed: Number(getCorrectFuckingValue(r, keys, 'speed')),
-        // power in watts
-        power: Number(getCorrectFuckingValue(r, keys, 'power')),
-        // temperature in celsius
-        temperature: Number(getCorrectFuckingValue(r, keys, 'temperature')),
-      }
-    });
-
+function getWorkoutData(records) {
   // calculate Normalized Power
   const rolling_averages_raised_to_fourth_power = records.map((_, i) => (i > 29
     // get previous 30 days worth of power and average them, then raise to 4th power
-    ? (records.slice(i - 30, i).reduce((acc, { power }) => acc + power, 0) / 30) ** 4
+    ? (records.slice(i - 30, i).reduce((acc, { power = 0 }) => acc + power, 0) / 30) ** 4
     : 0
   ))
-
+  
   const averaged_rolling_averages = rolling_averages_raised_to_fourth_power.reduce((acc, val) => acc + val, 0) / rolling_averages_raised_to_fourth_power.length
   const normalized_power = Number((averaged_rolling_averages ** 0.25).toFixed())
 
@@ -88,19 +110,19 @@ csv()
   const training_stress_score = Number(((records.length * normalized_power * intensity_factor) / (functional_threshold_power * 36)).toFixed())
   
   // basic workout data
-  const distance = Number((records[records.length - 1].distance).toFixed(2))
+  const distance = Number((_get(records, `${[records.length - 1]}.distance`, 0)).toFixed(2))
   const duration = records.length * 1000
   
   // total stats
   const recordsLength = records.length
   const finalIndex = recordsLength - 1
 
-  let stats = records.reduce((acc, { power, heart_rate, cadence, speed, altitude, temperature }, index) => ({
+  let stats = records.reduce((acc, { power = 0, heart_rate = 0, cadence = 0, speed = 0, altitude = 0, temperature = 0 }, index) => ({
     ...acc,
     ...minAvgMaxCalculations(acc, { power, heart_rate, cadence, speed, altitude, temperature }, index, finalIndex, recordsLength)
   }), {})
 
-  const workout_data = {
+  return {
     distance,
     duration,
     functional_threshold_power,
@@ -109,9 +131,4 @@ csv()
     training_stress_score,
     stats,
   }
-  
-  const endTime = Date.now()
-  
-  console.log('workout_data', JSON.stringify(workout_data, null, 2))
-  console.log(`${recordsLength} records in ${endTime - startTime} milliseconds`);
-})
+}
